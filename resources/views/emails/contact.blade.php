7edc8c0dd6f7ad0c74bf4d931fdeb3aea2af1abe
SutoIstvan <isutoi@gmail.com>
2024-10-25 14:09:01 +0200

    update

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form Submission</title>
+</head>
+<body>
+    <h1>Новое сообщение с формы обратной связи</h1>
+    <p><strong>Имя:</strong> {{ $data['name'] }}</p>
+    <p><strong>Email:</strong> {{ $data['email'] }}</p>
+    <p><strong>Сообщение:</strong> {{ $data['message'] }}</p>
+</body>
+</html>
